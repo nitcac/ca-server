182feae6f735145995433c947926e24f79d83341
hamadatakaki <i16hamadat@gmail.com>
2019-04-24 17:33:42 +0900

    :tada: Add ranking post function for heisei-tower-battle

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from server import api
+from server.models import Base, engine
 
 
 if __name__ == '__main__':
+    Base.metadata.create_all(engine)
     api.run(address='0.0.0.0', port=80)
--- server/views/htb_view.py
+++ server/views/htb_view.py
@@ -1,11 +1,38 @@
 from server import api
 from server.models.htb import HTBResult
+from server.models import SessionManager
 
 
-@api.route('/api')
+@api.route('/api/htb/ranking')
 class HTBRankingView:
     async def on_post(self, req, resp):
         data = await req.media()
-        response = {}
 
-        resp.media = response
+        if 'player_name' in data.keys() and 'score' in data.keys():
+            try:
+                score = int(data['score'])
+            except ValueError:
+                resp.media = {'message': 'Is the `score` numeric ?'}
+                resp.status_code = api.status_codes.HTTP_400
+                return
+
+            result = HTBResult(
+                player_name=data['player_name'],
+                score=score
+            )
+
+            with SessionManager() as session:
+                session.add(result)
+                session.commit()
+
+            resp.media = {'message': 'Good Request .'}
+            resp.status_code = api.status_codes.HTTP_201
+
+        else:
+            resp.media = {
+                'message': 'the paramator is not found:{}{} .'.format(
+                    int('player_name' not in data.keys()) * ' `player_name`',
+                    int('score' not in data.keys()) * ' `score`'
+                )
+            }
+            resp.status_code = api.status_codes.HTTP_400
