8e96220d43a70a1d820cf97eee93d7f42f7f9b70
hamadatakaki <i16hamadat@gmail.com>
2019-04-30 15:11:25 +0900

    :+1: Update View

--- server/views/htb_view.py
+++ server/views/htb_view.py
@@ -2,28 +2,29 @@ from server import api
 from server.models import SessionManager
 from server.models.htb import HTBResult
 
+import json
+
 
 @api.route('/api/htb/ranking')
 class HTBRankingView:
+    def on_get(self, req, resp):
+        with SessionManager() as session:
+            scores = session.query(HTBResult).order_by(HTBResult.score).all()
+            scores.reverse()
+
+        N = min(10, len(scores))
+            
+        resp.media = {'scores': [s.serialize() for s in scores[:N]]}
+        resp.status_code = api.status_codes.HTTP_200
+
     async def on_post(self, req, resp):
-        data = await req.media()
+        try:
+            data = await req.media()
+        except json.decoder.JSONDecodeError:
+            data = {}
 
-        if 'player_name' not in data.keys() or 'score' not in data.keys():
-            resp.media = {
-                'message': 'the paramator is not found:{}{} .'.format(
-                    int('player_name' not in data.keys()) * ' `player_name`',
-                    int('score' not in data.keys()) * ' `score`'
-                )
-            }
-            resp.status_code = api.status_codes.HTTP_400
-        else:
-            try:
-                score = int(data['score'])
-            except ValueError:
-                resp.media = {'message': 'Is the `score` numeric ?'}
-                resp.status_code = api.status_codes.HTTP_400
-                return
-
+        if 'player_name' in data.keys() and 'score' not in data.keys() and data['score'].isdecimal():
+            score = int(data['score'])
             result = HTBResult(
                 player_name=data['player_name'],
                 score=score
@@ -38,8 +39,9 @@ class HTBRankingView:
 
             N = min(10, len(scores))
             
-            resp.media = {
-                'message': 'Good Request .',
-                'results': [s.serialize() for s in scores[:N]]
-            }
+            resp.media = {'scores': [s.serialize() for s in scores[:N]]}
             resp.status_code = api.status_codes.HTTP_201
+
+        else:
+            resp.media = {'message': 'Invalid request body'}
+            resp.status_code = api.status_codes.HTTP_400
